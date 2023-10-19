e52724ba15564fc012af7c9e0818b852f99d8a56
Malaka98 <malaka.aruna14413@gmail.com>
2023-10-20 01:06:33 +0530

    fix

--- lib/src/screens/login/login.dart
+++ lib/src/screens/login/login.dart
@@ -48,74 +48,73 @@ class _LoginState extends State<Login> {
     return SafeArea(
       child: Scaffold(
           body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30.0),
-                    bottomRight: Radius.circular(30.0)),
-                image: DecorationImage(
-                    image: AssetImage("assets/login.png"), fit: BoxFit.none)),
-            height: MediaQuery.of(context).size.height / 2,
-          ),
-          Flexible(
-              child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 100.0),
-                const Padding(
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: Text("Log In to", style: TextStyle(fontSize: 24)),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 10.0, bottom: 20.0),
-                  child: Text(
-                    "Agri Grow",
-                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black.withOpacity(0.3),
-                              spreadRadius: 1,
-                              blurRadius: 8,
-                              offset: const Offset(0, 1))
-                        ]),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 20),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10.0),
-                          child: Text(
-                              style: TextStyle(
-                                fontSize: 14.00,
-                                color: Colors.grey.shade600,
-                              ),
-                              textAlign: TextAlign.start,
-                              "Please log in to your account to continue:"),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.all(24.0),
-                          child: LoginFrom(loginHandlerCB: loginHandler),
-                        ),
-                      ],
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30.0),
+                        bottomRight: Radius.circular(30.0)),
+                    image: DecorationImage(
+                        image: AssetImage("assets/login.png"), fit: BoxFit.none)),
+                height: MediaQuery.of(context).size.height / 2,
+              ),
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 100.0),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: Text("Log In to", style: TextStyle(fontSize: 24)),
                     ),
-                  ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 10.0, bottom: 20.0),
+                      child: Text(
+                        "Agri Grow",
+                        style: TextStyle(fontSize: 36, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black.withOpacity(0.3),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: const Offset(0, 1))
+                            ]),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 20),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0),
+                              child: Text(
+                                  style: TextStyle(
+                                    fontSize: 14.00,
+                                    color: Colors.grey.shade600,
+                                  ),
+                                  textAlign: TextAlign.start,
+                                  "Please log in to your account to continue:"),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.all(24.0),
+                              child: LoginFrom(loginHandlerCB: loginHandler),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ))
-        ],
-      )),
+              )
+            ],
+          )),
     );
   }
 }
