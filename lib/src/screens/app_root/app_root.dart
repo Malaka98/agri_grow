ef323297d4fe77516bea1ed5a38e39e022869e50
Malaka98 <malaka.aruna14413@gmail.com>
2023-10-20 22:02:52 +0530

    fix

--- lib/src/screens/app_root/app_root.dart
+++ lib/src/screens/app_root/app_root.dart
@@ -14,99 +14,15 @@ class _AppRootState extends State<AppRoot> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.center,
-                end: Alignment.bottomLeft,
-                colors: [
-              Colors.green.shade100,
-              Colors.green.shade50,
-              Colors.white70,
-              Colors.white
-            ])),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(left: 70.0, top: 70.0),
-              height: 450,
-              decoration: BoxDecoration(
-                  image: const DecorationImage(
-                      image: AssetImage("assets/app_root.png"),
-                      fit: BoxFit.fitWidth,
-                      opacity: 0.8),
-                  border: Border.all(width: 0, color: Colors.green.shade900),
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(20.0),
-                      bottomLeft: Radius.circular(20.0)),
-                  color: Colors.green.shade900),
-            ),
-            Container(
-              width: double.infinity,
-              height: 70,
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                    image: AssetImage("assets/app_root2.png"),
-                    fit: BoxFit.cover,
-                    opacity: 0.8),
-                color: Colors.green.shade900,
-                border: Border.all(width: 0, color: Colors.transparent),
-              ),
-              child: FittedBox(
-                alignment: Alignment.centerLeft,
-                child: InkWell(
-                  child: Container(
-                    width: 250,
-                    height: 60,
-                    decoration: BoxDecoration(
-                        border: Border.all(width: 0, color: Colors.white),
-                        borderRadius: const BorderRadius.only(
-                            topRight: Radius.circular(40.0),
-                            bottomRight: Radius.circular(40.0)),
-                        color: Colors.white),
-                    child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              "Get Started",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.green.shade800,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Icon(
-                              Icons.arrow_forward_rounded,
-                              size: 32,
-                              color: Colors.green.shade800,
-                            )
-                          ],
-                        )),
-                  ),
-                  onTap: () {
-                    context.push(Routes.login.path);
-                  },
-                ),
-              ),
-            ),
-            Flexible(
-                child: Container(
-              margin: const EdgeInsets.only(left: 150.0),
-              decoration: BoxDecoration(
-                  image: const DecorationImage(
-                    image: AssetImage("assets/app_root2.png"),
-                    fit: BoxFit.none,
-                  ),
-                  borderRadius:
-                      const BorderRadius.only(topLeft: Radius.circular(20.0)),
-                  border: Border.all(width: 0, color: Colors.transparent),
-                  color: Colors.transparent),
-            ))
-          ],
+          body: InkWell(
+        child: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/app_root.png"), fit: BoxFit.fill)),
         ),
+        onTap: () {
+          context.push(Routes.login.path);
+        },
       )),
     );
   }
